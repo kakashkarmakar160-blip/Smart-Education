f3759aaeda4ec216c9c343d0883def00c472f496
kakashkarmakar160-blip <kakashkarmakar160@gmail.com>
2026-09-08 12:26:04 +0530

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://wrrdwaqfzascwkwnhjbo.supabase.co";
+const SUPABASE_URL = "https://wrrdwqfzascwknhjbo.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Ce3UyZX_1rUmnDXfwuLOrQ_SJB7Cv52";
 
